eba2dd1c7c4e82583db0c1a1e4af88915a931ec9
grapeasy03 <121153854+grapeasy03@users.noreply.github.com>
2025-03-18 19:37:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,18 +2,30 @@ const express=require("express")
 
 const app=express()
 
-app.use("/",(req,res)=>{
-    res.send("Hello from main")
-})
+
 
 app.use("/hello",(req,res)=>{
     res.send("Hello from hello")
 })
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from server")
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from server")
+// })
+//only handlles get call
+app.get("/user",(req,res)=>{
+    res.send({firstName:"badri"})
 })
 
+app.post("/user",(req,res)=>{
+    console.log("Data saved to db")
+    res.send("Data saved to db")
+
+})
+
+app.delete("/user",(req,res)=>{
+    console.log("Data deleted from db")
+    res.send("Data deleted from db")
+})
 app.listen(7777,()=>{
-    console.log("server is successfully to port 7787")
+    console.log("server is successfully to port 7777")
 })
